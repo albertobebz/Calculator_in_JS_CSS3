8e9dee44e583ce478126101b77332475391ed5b0
Alberto Ferioli <alberto_ferioli@yahoo.it>
2015-07-23 12:35:10 +0100

    created the script.js file/linked to the page and collect all the keys in a var + create a for loop and perform operations

---
+++ jquery-script.js
@@ -0,0 +1,5 @@
+/* fadeOut the CSS3 shape when the window is resizing */
+
+$( window ).resize(function() {
+  $( "#talkbubble" ).fadeOut(600);
+});
--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-/* fadeOut the CSS3 shape when the window is resizing */
+// get all the keys button
+var keys = document.querySelectorAll('#calculator span');
 
-$( window ).resize(function() {
-  $( "#talkbubble" ).fadeOut(600);
-});
+// add the onclik event to all the keys and perform operations
+
+for(var i=0; i<keys.lenght; i++){
+  keys[i].onclick = function(e){
+    // get the input and button values
+    var input = document.querySelector('.screen');
+    var inputVal = input.innerHTML;
+    var btnVal = this.innerHTML;
+  }
+}
